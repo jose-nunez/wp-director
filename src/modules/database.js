4a074ae99e314b2f9333c68f0dadd3cfd473e947
Jose Nunez <josenunezdiaz@gmail.com>
2017-11-13 15:25:23 +1100

    Joins settings extracted from database and config file

---
+++ src/modules/database.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+
+let database;
+function c(db_name,db_user,db_pass){
+	if(database) return database;
+	else if(db_name && db_user && db_pass){
+		database = new Promise((resolve,reject)=>{
+			MongoClient.connect(`mongodb://${db_user}:${db_pass}@localhost:27017/${db_name}`,function(err, db){
+				assert.equal(err, null);
+				resolve(db);
+			});
+		});
+		return database;
+	}
+	else return Promise.reject(new Error('No credentials provided'));
+}
+
+class DB{
+	
+	constructor({db_name,db_user,db_pass}){
+		c(db_name,db_user,db_pass);
+	}
+
+	find(collection,query,projection){		
+		return c().then(db=>{
+			let cursor = db.collection(collection).find(query);
+			cursor.project(projection);
+			return cursor;
+		});
+	}
+
+	getStageSettings(site_name){
+		let query = site_name?{name: site_name}:null;
+		return this.find('stagesites',query,{'_id':false}).then(cursor=>cursor.next());
+		
+	}
+	
+}
+
+exports.DB = DB;
---
+++ src/modules/settings.js
@@ -0,0 +1,38 @@
+const YAML = require('yamljs');
+const {DB} = require('./database');
+const fs = require('fs');
+const path = require('path');
+
+
+function copyAttrs(prev,next){
+	if(!next) return prev;
+	else if(typeof next !== "object" || typeof prev !== "object") return next;
+	else{
+		let r = {};
+		let prev_keys = Object.keys(prev);
+		prev_keys.map(key=>{
+			r[key] = copyAttrs(prev[key],next[key])
+		});
+		let next_keys = Object.keys(next).filter(key=>prev_keys.indexOf(key)==-1);
+		next_keys.map(key=>{
+			r[key] = next[key];
+		});
+		return r;
+	}
+}
+
+function joinSettings(deaultSettings,newSettings){
+	return copyAttrs(deaultSettings,newSettings);
+}
+
+function getAppSettings(site_name){
+	let deault_settings = YAML.load(path.join(__dirname,'../config.yml'));
+	let dbconnection = deault_settings.dbconnection;
+	delete(deault_settings.dbconnection); // Will not go to the app
+	
+	let connector = new DB(dbconnection);
+	return connector.getStageSettings(site_name).then(site_settings=>joinSettings(deault_settings,site_settings));
+
+}
+
+exports.getAppSettings = getAppSettings;
